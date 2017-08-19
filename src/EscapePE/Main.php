a297f558f09b77044b6c289559456ca1d2d003df
trasta <i.taiki720@gmail.com>
2017-08-19 17:14:58 +0900

    データセーブ追加

--- src/EscapePE/Main.php
+++ src/EscapePE/Main.php
@@ -5,6 +5,8 @@ namespace EscapePE;
 # Base
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\Server;
 
 # Other
 use pocketmine\scheduler\PluginTask;
@@ -16,6 +18,41 @@ class Main extends PluginBase implements Listener{
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents(new Event($this), $this);
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new TimeTable($this), 1);
+		//$this->dataSave("trasta298");
+	}
+
+	//データをセーブする
+	public function dataSave($name){
+		if(isset($this->data[$name])){
+			$data = json_encode($this->data[$name]);
+		}else{
+			$data = json_encode($this->dataLoad($name));
+		}
+		$res = file_put_contents("SaveData/".$name.".json",$data);
+		if($res){
+			Server::getInstance()->getLogger()->info("".$name."のデータセーブが完了しました");
+		}else{
+			Server::getInstance()->getLogger()->info("".$name."のデータセーブに失敗しました");
+		}
+	}
+
+	//データをロードする
+	public function dataLoad($name){
+		$res = file_get_contents("SaveData/".$name.".json");
+		if($res){
+			Server::getInstance()->getLogger()->info($name."のデータロードが完了しました");
+			return json_decode($res);
+		}else{
+			Server::getInstance()->getLogger()->info($name."のデータが見つからなかったので新しいデータを作成しました");
+			return $this->getNewData();
+		}
+	}
+
+	public function getNewData(){
+		$data = [
+			"rank" => 1
+		];
+		return $data;
 	}
 
 	//エントリーリクエスト処理
