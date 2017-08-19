fbfec6b2fd53f07873129df9ed3dfc8d3710bd7d
trasta298 <i.taiki720@gmail.com>
2017-08-19 23:44:28 +0900

    ...

--- src/EscapePE/Main.php
+++ src/EscapePE/Main.php
@@ -18,36 +18,35 @@ class Main extends PluginBase implements Listener{
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents(new Event($this), $this);
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new TimeTable($this), 1);
-		//$this->dataSave("trasta298");
 	}
 
 	//データをセーブする
 	public function dataSave($name){
 		if(isset($this->data[$name])){
-			$data = json_encode($this->data[$name]);
+			$res = $this->data[$name]->dataSave();
 		}else{
 			$data = json_encode($this->dataLoad($name));
+			$res = file_put_contents("SaveData/".$name.".json",$data);
 		}
-		$res = file_put_contents("SaveData/".$name.".json",$data);
 		if($res){
-			Server::getInstance()->getLogger()->info("".$name."のデータセーブが完了しました");
+			Server::getInstance()->getLogger()->info("\033[1;34m".$name."のデータセーブが完了しました"."\033[0m");
 		}else{
-			Server::getInstance()->getLogger()->info("".$name."のデータセーブに失敗しました");
+			Server::getInstance()->getLogger()->info("\033[0;31m".$name."のデータセーブに失敗しました"."\033[0m");
 		}
 	}
 
 	//データをロードする
 	public function dataLoad($name){
-		$res = file_get_contents("SaveData/".$name.".json");
-		if($res){
-			Server::getInstance()->getLogger()->info($name."のデータロードが完了しました");
+		if(file_exists("SaveData/".$name.".json")){
+			$res = file_get_contents("SaveData/".$name.".json");
+			Server::getInstance()->getLogger()->info("\033[1;34m".$name."のデータロードが完了しました"."\033[0m");
 			return json_decode($res);
 		}else{
-			Server::getInstance()->getLogger()->info($name."のデータが見つからなかったので新しいデータを作成しました");
+			Server::getInstance()->getLogger()->info("\033[1;36m".$name."のデータが見つからなかったので新しいデータを作成しました"."\033[0m");
 			return $this->getNewData();
 		}
-	}
-
+    }
+    
 	public function getNewData(){
 		$data = [
 			"rank" => 1
--- src/EscapePE/PlayerData.php
+++ src/EscapePE/PlayerData.php
@@ -4,8 +4,44 @@ namespace EscapePE;
 
 class PlayerData {
 
+    private final $VERSION = 1;
+
+    public $name;
+    public $rank;
+
     function __construct(){
         #code...
+    }
+
+	//データをセーブする
+	public function dataSave(){
+		$data = $this->encodeJSON();
+		$res = file_put_contents("SaveData/".$name.".json",$data);
+		if($res){
+			Server::getInstance()->getLogger()->info("\033[1;34m".$name."のデータセーブが完了しました"."\033[0m");
+		}else{
+			Server::getInstance()->getLogger()->info("\033[0;31m".$name."のデータセーブに失敗しました"."\033[0m");
+		}
+	}
+
+	//データをロードする
+	public function dataLoad($name){
+		if(file_exists("SaveData/".$name.".json")){
+			$res = file_get_contents("SaveData/".$name.".json");
+			Server::getInstance()->getLogger()->info("\033[1;34m".$name."のデータロードが完了しました"."\033[0m");
+			return json_decode($res);
+		}else{
+			Server::getInstance()->getLogger()->info("\033[1;36m".$name."のデータが見つからなかったので新しいデータを作成しました"."\033[0m");
+			return $this->getNewData();
+		}
+    }
+    
+    public function encodeJSON(){
+        $data = [
+            "version" => $this->VERSION,
+            "rank" => $this->rank
+        ];
+        return json_encode($data);
     }
 
 }
